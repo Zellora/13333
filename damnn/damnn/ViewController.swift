0be1725aec382eaa090ace03ec7ea21f6ff99ac0
Кабинет 35 номер пк-9 <kabinet35nomerpk-9@MacBook-Pro-Kabinet-35-nomer-pk-9.local>
2022-11-10 15:58:53 +0300

    коммент

---
+++ damnn/damnn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  damnn
+//
+//  Created by  Кабинет 35 номер пк-9 on 06.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
